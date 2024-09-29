db32fb3640481ebd6f2d4736be84b46c8ba600e2
Martin Disch <martindisch@gmail.com>
2024-09-29 16:29:22 +0200

    Set up CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{args:?}");
+}
+
+/// A dead simple tool using ImageMagick to resize images.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the input directory.
+    input: PathBuf,
+
+    /// Path to the output directory.
+    output: PathBuf,
 }
